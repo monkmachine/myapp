550e7aab9d4ccce6caa168e79631db9d83b8bae6
monkmachine <36521886+monkmachine@users.noreply.github.com>
2018-06-22 20:31:19 +0100

    First express app

--- routes/transactions.js
+++ routes/transactions.js
@@ -7,32 +7,36 @@ const isAuth = require('../auth');
 
 /* GET transactions listing. */
 router.get('/', function (req, res) {
-     var reqToken = req.header("Authorization").replace('Bearer ', '');
-     console.log(reqToken);
+    var reqToken = req.header("Authorization").replace('Bearer ', '');
+    console.log(reqToken);
     isAuth(reqToken, function (error, googparse) {
         if (error) {
             res.status(401).end();
         };
-        
+
         getConnection(function (err, con) {
             if (err) throw err;
             var email = googparse.email;
-            que = 'CALL ep_ico.getusertransactions(\'' + email + '\', @token ,@p_userJSON);select @p_userJSON as p_userJSON;';
-            console.log(que);
-            con.query(que, function (err, results) {
-                if (err) throw err;
-                else
-                    con.release();
-                var datapackstr = results[2];
-                console.log(datapackstr);
-                if (datapackstr.RowDataPacket.p_userJSON == null) {
-                    res.status(401).end();
-                } else {
-                    var datapack = datapackstr.p_userJSON;
-                    res.json(datapack);
+            if (email == undefined) {
+                res.status(401).end()
+            } else {
+                que = 'CALL ep_ico.getusertransactions(\'' + email + '\', @token ,@p_userJSON);select @p_userJSON as p_userJSON;';
+                console.log(que);
+                con.query(que, function (err, results) {
+                    if (err) throw err;
+                    else
+                        con.release();
+                    var datapackstr = results[2];
+                    console.log(datapackstr);
+                    if (datapackstr.RowDataPacket.p_userJSON == null) {
+                        res.status(401).end();
+                    } else {
+                        var datapack = datapackstr.p_userJSON;
+                        res.json(datapack);
 
-                };
-            });
+                    };
+                });
+            }
         });
     });
 });
@@ -41,30 +45,35 @@ router.get('/', function (req, res) {
 /* GET transactions listing. */
 
 router.post('/', function (req, res) {
-            var reqToken = req.header("Authorization").replace('Bearer ', '');
-            console.log(reqToken);
-            isAuth(reqToken, function (error, googparse) {
-                if (error) {
-                    res.status(401).end();
-                };
-                    getConnection(function (err, con) {
-                    var email = googparse.email;
-                    console.log('email',email);
+    var reqToken = req.header("Authorization").replace('Bearer ', '');
+    console.log(reqToken);
+    isAuth(reqToken, function (error, googparse) {
+        if (error) {
+            res.status(401).end();
+        };
+        getConnection(function (err, con) {
+            var email = googparse.email;
+            if (email == undefined) {
+                res.status(401).end()
+                console.log('email', email);
+            } else {
+                console.log('email', email);
+                if (err) throw err;
+                var guid = uuidv4();
+                var p_userJSON = JSON.stringify(req.body);
+                var p_ipCountry = 'NA';
+                // parameters are p_userJSON, p_googleEmail, p_ipCountry, p_eventId
+                que = 'CALL ep_ico.createproposedtransaction(\'' + p_userJSON + '\',\'' + email + '\',\'' + p_ipCountry + '\',\'' + guid + '\', @token, @p_return_text, @p_return_code ); select @p_return_text as return_text, @p_return_code as returncode';
+                console.log('>> que: ', que);
+                con.query(que, function (err, results) {
                     if (err) throw err;
-                    var guid = uuidv4();
-                    var p_userJSON = JSON.stringify(req.body);
-                    var p_ipCountry = 'NA';
-                    // parameters are p_userJSON, p_googleEmail, p_ipCountry, p_eventId
-                    que = 'CALL ep_ico.createproposedtransaction(\'' +p_userJSON   + '\',\'' + email + '\',\'' + p_ipCountry + '\',\'' + guid + '\', @token, @p_return_text, @p_return_code ); select @p_return_text as return_text, @p_return_code as returncode';
-                    console.log('>> que: ', que);
-                    con.query(que, function (err, results) {
-                        if (err) throw err;
-                        con.release();
-                        console.log('>> results: ', results);
-                        res.json(results[1]);
-                    });
+                    con.release();
+                    console.log('>> results: ', results);
+                    res.json(results[1]);
                 });
-            });
-        }); 
+            }
+        });
+    });
+});
 
-            module.exports = router;
+module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -7,30 +7,34 @@ const isAuth = require('../auth');
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-     var reqToken = req.header("Authorization").replace('Bearer ', '');
-     console.log(reqToken);
+    var reqToken = req.header("Authorization").replace('Bearer ', '');
+    console.log(reqToken);
     isAuth(reqToken, function (error, googparse) {
         if (error) {
             res.status(401).end();
         };
-        
+
         getConnection(function (err, con) {
             if (err) throw err;
             var email = googparse.email;
-            que = 'CALL ep_ico.getUser(?, @token ,@p_userJSON);select @p_userJSON as p_userJSON;';
-            con.query(que, [email,"xxx"], function (err, results) {
-                if (err) throw err;
-                else
-                    con.release();
-                var datapackstr = results[1];
-                if (datapackstr.p_userJSON == null) {
-                    res.status(401).end();
-                } else {
-                    var datapack = datapackstr.p_userJSON;
-                    res.json(datapack);
+            if (email == undefined) {
+                res.status(401).end()
+            } else {
+                que = 'CALL ep_ico.getUser(?, @token ,@p_userJSON);select @p_userJSON as p_userJSON;';
+                con.query(que, [email, "xxx"], function (err, results) {
+                    if (err) throw err;
+                    else
+                        con.release();
+                    var datapackstr = results[1];
+                    if (datapackstr.p_userJSON == null) {
+                        res.status(401).end();
+                    } else {
+                        var datapack = datapackstr.p_userJSON;
+                        res.json(datapack);
 
-                };
-            });
+                    };
+                });
+            }
         });
     });
 });
@@ -39,32 +43,37 @@ router.get('/', function (req, res) {
 /* GET users listing. */
 
 router.post('/', function (req, res) {
-            var reqToken = req.header("Authorization").replace('Bearer ', '');
-            console.log(reqToken);
-            isAuth(reqToken, function (error, googparse) {
-                if (error) {
-                    res.status(401).end();
-                };
-                    getConnection(function (err, con) {
-                    var email = googparse.email;
-                    console.log('email',email);
-                    if (err) throw err;
-                    var guid = uuidv4();
-                    var p_userJSON = JSON.stringify(req.body);
-                    var p_ipCountry = 'UK';
-                    console.log('req', p_userJSON);
-                    console.log('req', guid);
-                    // parameters are p_userJSON, p_googleEmail, p_ipCountry, p_eventId
-                    que = 'CALL ep_ico.createUser(\'' + p_userJSON + '\',\'' + email + '\',\'' + p_ipCountry + '\',\'' + guid + '\', @token, @p_return_text, @p_return_code ); select @p_return_text as p_return_text, @p_return_code as p_return_code';
-                    console.log('>> que: ', que);
-                    con.query(que, function (err, results) {
-                        if (err) throw err;
-                        con.release();
-                        console.log('>> results: ', results);
-                        res.json(results[1]);
-                    });
-                });
-            });
-        }); 
+    var reqToken = req.header("Authorization").replace('Bearer ', '');
+    console.log(reqToken);
+    isAuth(reqToken, function (error, googparse) {
+        if (error) {
+            res.status(401).end();
+        };
 
-            module.exports = router;
+        getConnection(function (err, con) {
+            var email = googparse.email;
+            if (email == undefined) {
+                res.status(401).end()
+            } else {
+                console.log('email', email);
+                if (err) throw err;
+                var guid = uuidv4();
+                var p_userJSON = JSON.stringify(req.body);
+                var p_ipCountry = 'UK';
+                console.log('req', p_userJSON);
+                console.log('req', guid);
+                // parameters are p_userJSON, p_googleEmail, p_ipCountry, p_eventId
+                que = 'CALL ep_ico.createUser(\'' + p_userJSON + '\',\'' + email + '\',\'' + p_ipCountry + '\',\'' + guid + '\', @token, @p_return_text, @p_return_code ); select @p_return_text as p_return_text, @p_return_code as p_return_code';
+                console.log('>> que: ', que);
+                con.query(que, function (err, results) {
+                    if (err) throw err;
+                    con.release();
+                    console.log('>> results: ', results);
+                    res.json(results[1]);
+                });
+            }
+        });
+    });
+});
+
+module.exports = router;
